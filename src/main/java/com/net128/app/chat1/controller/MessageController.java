1a81b6b2125e90087da0233a50c6cb5b9d5570bc
mike-seger <mss@unknown.com>
2018-04-15 01:53:41 +0200

    allow 100M upload

--- src/main/java/com/net128/app/chat1/controller/MessageController.java
+++ src/main/java/com/net128/app/chat1/controller/MessageController.java
@@ -16,7 +16,6 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.inject.Inject;
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.OutputStream;
