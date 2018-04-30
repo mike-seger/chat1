ae7b1b8001bbc61bb761e86a0a56f05cd4f6fe29
mike-seger <mss@unknown.com>
2018-05-01 01:29:18 +0200

    added artillery script

---
+++ src/test/scripts/artillery.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [[ ! -x artillery  ]] ; then
+	echo "Install applications from:"
+	echo "https://artillery.io/docs/getting-started/"
+	exit 1
+fi
+
+ulimit -n 2048
+ulimit -u 2048
+mvn spring-boot:run >/tmp/out.log &
+
+artillery quick --count 100 -n 200 http://user:password@localhost:18090/messages
